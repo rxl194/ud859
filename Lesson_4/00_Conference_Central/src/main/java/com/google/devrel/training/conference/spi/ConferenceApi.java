fe78e1064afc9be45663d63d65e806203bafa479
rxl194 <Rendong_Lin@yahoo.com>
2015-12-03 08:28:35 -0800

    Lesson_4

--- Lesson_4/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ Lesson_4/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -255,20 +255,20 @@ public class ConferenceApi {
 
         // Filter on city
         query = query.filter("city =", "London");
-/*
         // query = query.filter("city =", "Default City");
 
         // Add a filter for topic = "Medical Innovations"
         query = query.filter("topics =", "Medical Innovations");
 
         // Add a filter for maxAttendees
-        query = query.filter("maxAttendees >", 8);
+        query = query.filter("maxAttendees >", 10);
+/*
         query = query.filter("maxAttendees <", 10).order("maxAttendees").order("name");
+        */
 
         // Add a filter for month {unindexed composite query}
         // Find conferences in June
         query = query.filter("month =", 6);
-        */
 
         // multiple sort orders
 
